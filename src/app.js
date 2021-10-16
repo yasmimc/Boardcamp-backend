afea76320017747c468abc776c158b8e25798651
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-10-16 18:59:37 -0300

    Added customers CRUD

--- src/app.js
+++ src/app.js
@@ -22,11 +22,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ------------------------------ * CATEGORIES CRUD *------------------------------
+
 app.get("/categories", async (req, res) => {
 	try {
 		const result = await connection.query(`SELECT * FROM categories;`);
 		res.status(200).send(result.rows);
-	} catch (err) {
+	} catch (error) {
 		res.sendStatus(500);
 	}
 });
@@ -57,10 +59,12 @@ app.post("/categories", async (req, res) => {
 			]);
 			res.sendStatus(201);
 		}
-	} catch (err) {
+	} catch (error) {
 		res.sendStatus(500);
 	}
 });
+
+// ------------------------------ * GAMES CRUD *------------------------------
 
 app.get("/games", async (req, res) => {
 	try {
@@ -75,7 +79,7 @@ app.get("/games", async (req, res) => {
 		}
 		const result = await connection.query(`SELECT * FROM games`);
 		res.status(200).send(result.rows);
-	} catch (err) {
+	} catch (error) {
 		res.sendStatus(500);
 	}
 });
@@ -111,7 +115,126 @@ app.post("/games", async (req, res) => {
 			[name, image, stockTotal, categoryId, pricePerDay]
 		);
 		res.sendStatus(201);
-	} catch (err) {
+	} catch (error) {
+		res.sendStatus(500);
+	}
+});
+
+// ------------------------------ * CUSTOMERS CRUD *------------------------------
+
+app.get("/customers", async (req, res) => {
+	try {
+		const cpf = req.query.cpf;
+		if (cpf) {
+			const result = await connection.query(
+				`SELECT * FROM customers WHERE cpf = $1`,
+				[cpf]
+			);
+			res.status(200).send(result.rows);
+			return;
+		}
+
+		const result = await connection.query(`SELECT * FROM customers`);
+		res.status(200).send(result.rows);
+	} catch (error) {
+		res.sendStatus(500);
+	}
+});
+
+app.get("/customers/:id", async (req, res) => {
+	try {
+		const customerId = req.params.id;
+		const result = await connection.query(
+			`SELECT * FROM customers WHERE id = $1`,
+			[customerId]
+		);
+
+		if (result.rowCount === 0) {
+			res.sendStatus(404);
+			return;
+		}
+
+		res.status(200).send(result.rows);
+	} catch (error) {
+		res.sendStatus(500);
+	}
+});
+
+app.post("/customers", async (req, res) => {
+	try {
+		const newCustomer = req.body;
+		const { name, phone, cpf, birthday } = newCustomer;
+
+		if (!customerIsValid(newCustomer)) {
+			res.sendStatus(400);
+			return;
+		}
+		const customerAlredyExists = await dataAlredyExists(
+			"customers",
+			"cpf",
+			cpf
+		);
+		if (customerAlredyExists) {
+			res.sendStatus(409);
+			return;
+		}
+
+		await connection.query(
+			`INSERT INTO customers (name, phone, cpf, birthday) VALUES($1, $2, $3, $4)`,
+			[name, phone, cpf, birthday]
+		);
+		res.sendStatus(201);
+	} catch (error) {
+		res.sendStatus(500);
+	}
+});
+
+app.put("/customers/:id", async (req, res) => {
+	try {
+		const customerId = req.params.id;
+		const customer = await connection.query(
+			`SELECT * FROM customers WHERE id = $1`,
+			[customerId]
+		);
+
+		if (customer.rowCount === 0) {
+			res.sendStatus(404);
+			return;
+		}
+
+		const updatedCustomer = req.body;
+		const { name, phone, cpf, birthday } = updatedCustomer;
+
+		if (!customerIsValid(updatedCustomer)) {
+			res.sendStatus(400);
+			return;
+		}
+		const updatedCustomerAlredyExists = await dataAlredyExists(
+			"customers",
+			"cpf",
+			cpf
+		);
+
+		const thisIsAnotherCustomerCpf =
+			updatedCustomerAlredyExists &&
+			updatedCustomerAlredyExists.id !== customerId;
+
+		if (thisIsAnotherCustomerCpf) {
+			res.sendStatus(409);
+			return;
+		}
+		await connection.query(
+			`UPDATE customers SET 
+				name = $1,
+				phone = $2,
+				cpf = $3,
+				birthday = $4
+			WHERE id = $5`,
+			[name, phone, cpf, birthday, customerId]
+		);
+
+		res.sendStatus(200);
+	} catch (error) {
 		res.sendStatus(500);
 	}
 });
@@ -123,6 +246,27 @@ async function dataAlredyExists(table, columnName, value) {
 		`SELECT * FROM ${table} WHERE ${columnName} = $1;`,
 		[value]
 	);
-	if (existentData.rowCount !== 0) return true;
+	if (existentData.rowCount !== 0) return existentData.rows;
 	return false;
+}
+
+function dateIsValid(date) {
+	const checkDate = new Date(date);
+	return checkDate instanceof Date && !isNaN(checkDate);
+}
+
+function customerIsValid(newCustomer) {
+	const { phone, cpf, birthday } = newCustomer;
+
+	const cpfRegex = /^\d{3}.?\d{3}.?\d{3}-?\d{2}$/;
+	const phoneRegex = /(^[0-9]{2})(\s|-)?(9?[0-9]{4})-?([0-9]{4}$)/;
+	const dateRegex = /([0-9]{4})-?([0-9]{2})-?([0-9]{2})/;
+
+	const newCustomerIsValid =
+		cpfRegex.test(cpf) &&
+		phoneRegex.test(phone) &&
+		dateRegex.test(birthday) &&
+		dateIsValid(birthday);
+
+	return newCustomerIsValid;
 }
